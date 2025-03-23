a3d5921e8fea4bf8ffb43356a587c0a05d01aab6
Ndoffe Saliou Ndiaye <ndoffesaliou@gmail.com>
2025-03-23 17:07:36 +0000

    Ajout des fichiers du portofolio

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
